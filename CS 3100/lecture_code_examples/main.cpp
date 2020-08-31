1851eda14c4568af7dceacf7090d14ace0fa4dfb
Ryan Arnold <arnold.227@wright.edu>
2020-08-31 16:41:43 -0400

    added examples from lecture on 8/31/20.

--- CS 3100/lecture_code_examples/main.cpp
+++ CS 3100/lecture_code_examples/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
+#include <fstream>
+#include <cassert>
+#include <string>
 
 //template for IO_example
 void IO_example() ;
 void IO_manip() ;
+void read_write_textFiles() ;
+void assert_test();
+
+using namespace std ;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    /*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
     //IO_example() ;
-    IO_manip() ;
-    /*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+    //IO_manip() ;
+    read_write_textFiles() ;
     return EXIT_SUCCESS;
 }
 
@@ -38,4 +44,42 @@ void IO_manip(){
     std::cout<<std::scientific;
     std::cout<<std::setprecision(2);
     std::cout<<"("<<std::setw(20)<<y<<")"<<std::endl;
+}
+
+void read_write_textFiles()
+{
+    ifstream input;
+    ofstream output;
+
+    input.open("inputs/inputData.txt");
+
+    assert (!input.fail()) ;
+
+    output.open("outputs/OutputData.txt");
+
+    assert(!output.fail()) ;
+
+    output<<fixed<<setprecision(2);
+
+    double x ;
+    int linectr = 1 ;
+    while(input>>x)
+    {
+        output << x << endl ;
+        cout<<"read line "<< linectr <<endl;
+
+        linectr++ ;
+    }
+    cout<<"finished reading file."<<endl;
+
+    input.close();
+    output.close();
+}
+
+void assert_test()
+{
+    int i = 10 ;
+    assert(i >5) ;
+    cout<<"next bit of code, test passed"<<endl;
+
 }
