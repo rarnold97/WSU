91c591b5fba7548b6200d740f2eda08a35b23945
Ryan Arnold <arnold.227@wright.edu>
2020-08-31 15:35:13 -0400

    added examples from lecture on 8/29/20.

--- CS 3100/lecture_code_examples/main.cpp
+++ CS 3100/lecture_code_examples/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
+
+//template for IO_example
+void IO_example() ;
+void IO_manip() ;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    /*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+    //IO_example() ;
+    IO_manip() ;
+    /*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
     return EXIT_SUCCESS;
+}
+
+void IO_example() {
+    int i, j ;
+    std::cout << "enter value of x" << std::endl ;
+    std::cin >> i ;
+
+    for (j=0; j<i; j++)
+    {
+        std::cout<< "Welcome to CS 3100" <<std::endl ;
+    }
+}
+
+void IO_manip(){
+    int x = 201 ; 
+    double y = 342.123456789 ; 
+    std::cout << "(" << std::setw(20) << x << ")" << std::endl;
+    std::cout << std::setprecision(8);
+    std::cout<<"("<<std::setw(20)<<y<<")"<<std::endl;
+    std::cout<<std::fixed ;
+    std::cout<<std::setprecision(2);
+    std::cout<<"("<<std::setw(20)<<y<<")"<<std::endl;
+    std::cout<<std::scientific;
+    std::cout<<std::setprecision(2);
+    std::cout<<"("<<std::setw(20)<<y<<")"<<std::endl;
 }
