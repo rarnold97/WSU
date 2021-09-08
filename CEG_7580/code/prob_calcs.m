659f1665fada016c9c99f6973233548343ed69e8
ryanm <ryanm@DESKTOP-APAI6EK>
2021-09-08 19:23:37 -0400

    first version of probability function calculator.

--- CEG_7580/code/prob_calcs.m
+++ CEG_7580/code/prob_calcs.m
@@ -1,4 +1,8 @@
 function P = prob_calcs(M, varargin)
+    %%
+    % accepts a 2D matrix of image values and a variable lengthed arg for 'bitness'
+    % defaults to assuming 8-bit image
+    % returns a struct with probability information
 
     P = struct() ; 
 
@@ -9,8 +13,26 @@ function P = prob_calcs(M, varargin)
 
     L = 2^bits - 1 ; 
     Levels = 0:1:L ; 
+    dims = size(M) ; 
 
+    %assuming that the matrix is 2 dimensional
+    NM = dims(1) * dims(2) ; 
     K = length(Levels) ; 
+    hk = zeros(1, K)
 
+    for i - 1:K
+        hk(i) = sum(sum(M(M==Levels(i)))) ; 
+    end
+
+    pr = hk ./ NM ; 
+
+    Sk = zeros(1, K) ; 
+
+    for i = 1:K
+        Sk(i) = L * sum(pr(1:k)) ; 
+    end
+
+    P.Sk = Sk ; 
+    P.pr = pr ; 
 
 end
