3c1fb92779add47b8aeb53ded9faf0d63c4d2027
Ryan Arnold <ryanmarnold97@gmail.com>
2021-09-25 01:33:33 -0400

    finished problem 4, which was the hardest one.

--- CEG_7580/projects/project3/Problem4.m
+++ CEG_7580/projects/project3/Problem4.m
@@ -29,8 +29,12 @@ close all
 
         I_double = im2double(I) ; 
 
-        %apply laplacian
+        %grab required function handles for spatial processing
         fun_lap = get_filter_handle('laplace') ; 
+        fun_gradient = get_filter_handle('sobel');
+        fun_box = get_filter_handle('box') ;
+
+        %apply laplacian, part b)
         I_laplace_double = nlfilter(I_double, [3,3], fun_lap) ; 
 
         %shift values back to 0 - 255
@@ -41,15 +45,72 @@ close all
         imshow(I_laplace_int)
         title('Laplacian of Original Image')
 
+        % this is the extra step that the book does to obtain the blurred image in c)
+        %modify laplacian by virtue of smoothing and masking (we will use gaussian for lowpass here)
+        
+        I_gradient_double = nlfilter(I_double, [3,3], fun_gradient) ; 
+
+        I_grad_smooth_double = imgaussfilt(I_gradient_double) ;
+
+        I_lap_times_grad_double = I_grad_smooth_double .* I_laplace_double ; 
+
         % add the laplace to the original image
-        I_lap_plus_orig_int = shift_image_values(I_laplace_int + I) ; 
+        I_lap_plus_orig_int = shift_image_values(I_lap_times_grad_double + I_double) ; 
 
         subplot(2,4, 3)
         imshow(I_lap_plus_orig_int)
         title('Laplace + Original')
-
         
+        %commence part d) hommies
+        
+        %sobel gradient was already computed previously , so just display it 
+        I_sobel_int = shift_image_values(I_gradient_double) ; 
 
+        subplot(2,4, 4)
+        imshow(I_sobel_int)
+        title('Sobel Gradient Image')
+
+        %part e) baby
+        %now smooth the sobel gradient image using a 5x5 box filter (default is 5x5 in filter handle)
+        I_smooth_sobel_double = nlfilter(I_gradient_double, [5,5], fun_box) ; 
+        I_smooth_sobel_int = shift_image_values(I_smooth_sobel_double) ; 
+
+        %display smoothed sobel gradient image
+        subplot(2,4, 5)
+        imshow(I_smooth_sobel_int)
+        title('5X5 Box Filtered Sobel Gradient Image')
+
+        % part f) home stretch 
+        % multiply the result of part b) with part e)
+        Lap_times_smooth_sobel_double = I_smooth_sobel_double .* I_laplace_double ; 
+        Lap_times_smooth_sobel_int = shift_image_values(Lap_times_smooth_sobel_double);
+
+        %display results
+        subplot(2,4, 6)
+        imshow(Lap_times_smooth_sobel_int) ;
+        title('Product of Laplace and Smoothed Gradient Image')
+
+        % part g) I can almost taste it ...
+        % add part f) to original image
+        I_orig_plus_f_double = Lap_times_smooth_sobel_double + I_double ; 
+        I_orig_plus_f_int = shift_image_values(I_orig_plus_f_double) ; 
+
+        %display part f results
+        subplot(2,4, 7)
+        imshow(I_orig_plus_f_int)
+        title('Part f) Mask Plus Original')
+
+        % part h) do the power law transform as the last step
+        % perform power law using function on previous result
+        I_final_int = power_xform(I_orig_plus_f_double, c_power, gamma_power);
+
+        %display final results
+
+        subplot(2,4, 8)
+        imshow(I_final_int)
+        title('Final Image after Power Xform')
+
+        % BOOM TOUCHDOWN !
     end
 
 end
--- CEG_7580/projects/project3/power_xform.m
+++ CEG_7580/projects/project3/power_xform.m
@@ -1,9 +1,16 @@
-function I_xform = power_xform(I, c, gamma)
+function I_xform = power_xform(I, c, gamma, varargin)
     % performs power law tranformation on an input image
     % I -> input image dataset.  gray level
     % c -> coefficient multiplier
     % gamma -> exponential coefficient
+    % optional *
+    % arg1 -> return as shifted int, or just double
         
+        asInt = true ;
+        if nargin > 3
+            asInt = varargin{1};
+        end 
+
         % make sure data isnt integer.  Convert to double if it is
         test_type_val = I(1,1) ; 
         if ~isa(test_type_val, 'double')
@@ -11,8 +18,11 @@ function I_xform = power_xform(I, c, gamma)
         end
         
         % apply xform
-        I_xform = c*I.^gamma ;
-        %linearly scale data back to [0-255]
-        I_xform = shift_image_values(I_xform) ; 
+        I_xform = real(c*I.^gamma) ;
+
+        if asInt
+            %linearly scale data back to [0-255], if user so chooses
+            I_xform = shift_image_values(I_xform) ; 
+        end
         
     end
